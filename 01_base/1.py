3ef3585bf56a14a7b4cbf5e931fa920009443327
gxc <1003823149@qq.com>
2026-03-24 09:05:53 +0800

    feat: 修改python

---
+++ 01_base/1.py
@@ -0,0 +1,11 @@
+a1 = 'abcdefghijklmn'
+a2 = list()
+a2 = [1,2,3,4,5,6,7,8,9,10]
+a3 = a2[::-1]
+a4 = [1,2,3,4,5]
+a4.reverse()
+a5 = [1,1,1,1]
+a5.extend([2,2,2])
+# print(a5)
+a6 = tuple(a5)
+print(a6, type(a6), a6.index(1), a6.count(1))
